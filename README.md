768f1a2c8430a6a8e84cb2aeb20dedd554898402
18adrianoh <adrianohernandez2000gmail.com>
2020-05-15 12:32:47 -0700

    poc for pygame i guess i need to learn sockets though

--- README.md
+++ README.md
@@ -1,2 +1,6 @@
 # Tutte
 Un Juego De Tutte Para Jugar Con La Familia
+
+Run install-win to install on windows or install to install on mac (not yet up and running.)
+
+For the future I might consider running this in a virtual environment or something but not yet.
---
+++ client.py
@@ -0,0 +1,28 @@
+# un juego de tute para jugar online con la familia
+# client has some basic functionality for displaying cards and helping you make choices which are then sent to the server
+import pygame as pg
+import sys
+
+WIDTH = 500
+HEIGHT = 500
+COLOR_WHITE = (255, 255, 255)
+
+window = pg.display.set_mode((WIDTH, HEIGHT))
+pg.display.set_caption("client")
+
+client_id = 0
+
+def draw(window, color):
+    window.fill(color)
+    pg.display.update()
+
+def main():
+    running = True
+    while running:
+        draw(window, COLOR_WHITE)
+        for event in pg.event.get():
+            if event.type == pg.QUIT:
+                running = False
+                pg.display.quit()
+                pg.quit()
+main()
