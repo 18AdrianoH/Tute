27434ce93efd2666b60691a2a390d501f1707920
18adrianoh <adrianohernandez2000gmail.com>
2020-05-15 18:51:16 -0700

    working real time interaction where I press spacebar and you get a message :D

--- client.py
+++ client.py
@@ -6,55 +6,30 @@
 # it will have basic timers to make sure people don't take too long
 # and it will have a simple installer to install python and the necessary dependencies on mac, windows, or linux
 
-#import pygame as pg
+import pygame
 import socket
 
-# WIDTH = 500
-# HEIGHT = 500
-# COLOR_WHITE = (255, 255, 255)
+WIDTH = 500
+HEIGHT = 500
+COLOR_WHITE = (255, 255, 255)
 
-# class Sprite:
-#     pass
-# class Card(Sprite):
-#     pass
-
-# window = pg.display.set_mode((WIDTH, HEIGHT))
-# pg.display.set_caption("client")
-
-# client_id = 0
-
-# def draw(window, color):
-#     window.fill(color)
-#     pg.display.update()
-
-# def main():
-#     running = True
-#     while running:
-#         draw(window, COLOR_WHITE)
-#         for event in pg.event.get():
-#             if event.type == pg.QUIT:
-#                 running = False
-#                 pg.display.quit()
-#                 pg.quit()
-# main()
-
-# TODO left off right after 3 will now do 4
 class Network:
     def __init__(self):
         self.client = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
         self.server = "10.0.0.211"
         self.port = 5555
-        self.addr = (self.server, self.port)
         self.id = self.connect()
-        print(self.id)
 
     def connect(self):
         try:
-            self.client.connect(self.addr)
+            self.client.connect((self.server, self.port))
         except Exception as exc:
             print("Failed to Connect, exception information below.")
             print(str(exc))
         return self.client.recv(2048).decode(encoding="utf-8")
+    
+    def quit(self):
+        self.client.close()
     
     def send(self, data):
         try:
@@ -63,6 +38,29 @@ class Network:
             print(err)
         return self.client.recv(2048).decode()
 
-net = Network()
-sent = net.send("hello, this is a message from the client")
-print(sent)
+window = pygame.display.set_mode((WIDTH, HEIGHT))
+pygame.display.set_caption("client")
+
+def draw(window, color):
+    window.fill(color)
+    pygame.display.update()
+
+def main(client_id=0):
+    net = Network()
+
+    running = True
+    while running:
+        draw(window, COLOR_WHITE)
+        for event in pygame.event.get():
+            # user can quit just by closing the window
+            if event.type == pygame.QUIT:
+                running = False
+                pygame.display.quit()
+                net.quit()
+                pygame.quit()
+            # we set it up for testing so that if you set up a space-bar you send a request
+            elif event.type == pygame.KEYDOWN and event.key == pygame.K_SPACE:
+                net.send("request from client with id {}".format(str(client_id)))
+    
+if __name__ == "__main__":
+    main(client_id=0)
